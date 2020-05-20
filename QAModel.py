c3cdbee0dc19a6c8c5802e1788c6d8b01c2c78b8
John Chen <john.person1@gmail.com>
2020-05-20 18:43:05 -0400

    add pubmedqa model

--- QAModel.py
+++ QAModel.py
@@ -10,10 +10,19 @@ from allennlp.data.fields import LabelField, TextField
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 from allennlp.data.tokenizers import Token, Tokenizer, WhitespaceTokenizer
 from allennlp.models import Model
-from allennlp.modules import TextFieldEmbedder, Seq2VecEncoder
+from allennlp.modules import TextFieldEmbedder, Seq2VecEncoder, Seq2SeqEncoder
 from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
 from allennlp.modules.token_embedders import Embedding
 from allennlp.modules.seq2vec_encoders import BagOfEmbeddingsEncoder, CnnEncoder
+
+
+'''trying to use a gated network, we will need many things. 
+Setting up the loss/getting the sizes correct. But more importantly, we will need 
+to get the evaluation metrics working, and teacher forcing etc.
+'''
+from allennlp.modules.seq2seq_encoders import GatedCnnEncoder
+
+
 from allennlp.nn import util
 from allennlp.training.metrics import CategoricalAccuracy, Auc
 from allennlp.training.optimizers import AdamOptimizer
@@ -40,7 +49,10 @@ class QAClassifier(Model):
 
     def forward(self,
                 text: Dict[str, torch.Tensor],
-                label: torch.Tensor) -> Dict[str, torch.Tensor]:
+                label: torch.Tensor,
+                long_answer: torch.Tensor,
+
+                ) -> Dict[str, torch.Tensor]:
         # Shape: (batch_size, num_tokens, embedding_dim)
         embedded_text = self.embedder(text)
         # Shape: (batch_size, num_tokens)
@@ -62,4 +74,50 @@ class QAClassifier(Model):
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
         return {"accuracy": self.accuracy.get_metric(reset)}
 
+
+class QATranslation(Model):
+    def __init__(self,
+                 vocab: Vocabulary,
+                 embedder: TextFieldEmbedder,
+                 encoder: Seq2VecEncoder):
+        super().__init__(vocab)
+        self.embedder = embedder
+        self.encoder = encoder
+        print("num labels is")
+        print(vocab.get_vocab_size("labels"))
+
+        num_labels = vocab.get_vocab_size("labels")
+
+        print("it is probably since we have a seq2seq in reality")
+
+        self.classifier = torch.nn.Linear(encoder.get_output_dim(), num_labels)
+        self.accuracy = CategoricalAccuracy()
+        self.auc = Auc()
+
+    def forward(self,
+                text: Dict[str, torch.Tensor],
+                label: torch.Tensor,
+                long_answer: torch.Tensor,
+
+                ) -> Dict[str, torch.Tensor]:
+        # Shape: (batch_size, num_tokens, embedding_dim)
+        embedded_text = self.embedder(text)
+        # Shape: (batch_size, num_tokens)
+        mask = util.get_text_field_mask(text)
+        # Shape: (batch_size, encoding_dim)
+        encoded_text = self.encoder(embedded_text, mask)
+        # Shape: (batch_size, num_labels)
+        logits = self.classifier(encoded_text)
+        # Shape: (batch_size, num_labels)
+        probs = torch.nn.functional.softmax(logits)
+        # Shape: (1,)
+        loss = torch.nn.functional.cross_entropy(logits, label)
+        self.accuracy(logits, label)
+        preds = logits.argmax(-1)
+        # self.auc(preds, label)
+        output = {'loss': loss, 'probs': probs}
+        return output
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {"accuracy": self.accuracy.get_metric(reset)}
 
